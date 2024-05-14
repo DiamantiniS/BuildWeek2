7057d921a20f64e9dffe9012cc7affe84a07ee36
AlessandroBianco00 <alessandrobianco2000@gmail.com>
2024-05-14 17:46:41 +0200

    Inserimento playlist nella pagina index con js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,45 @@
+// PLAYLIST SUGGERITE
+
+const showPlaylist = function (object) {
+  let numeroConsigliati = 10;
+  arrayPlaylist = object.data;
+  const divPlaylist = document.getElementById("div-playlist");
+  divPlaylist.innerHTML = "";
+
+  for (let i = 0; i < numeroConsigliati; i++) {
+    let singolaPlaylist = document.createElement("div");
+    singolaPlaylist.classList.add("d-flex", "gap-3");
+    singolaPlaylist.innerHTML = `<img src="${arrayPlaylist[i].album.cover_small}" class="object-fit-cover p-1"/>
+    <div class="text-white d-none d-md-block">
+      <h5>${arrayPlaylist[i].title_short}</h5>
+      <p>${arrayPlaylist[i].artist.name}</p>
+    </div>`;
+    divPlaylist.appendChild(singolaPlaylist);
+  }
+};
+
+const recomendedPlaylist = function () {
+  fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=pop", {})
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Errore caricamento playlist");
+      }
+    })
+    .then((obj) => {
+      showPlaylist(obj);
+      console.log(obj);
+    })
+    .catch((err) => {
+      console.log("Error", err);
+    });
+};
+
+recomendedPlaylist();
+
+// MAIN
+
 const fullHomepage = function (object) {
   const arrayHome = object.data;
 };
